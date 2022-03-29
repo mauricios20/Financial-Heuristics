e21a2b1ca36781645943ecb2a5af0ceb35ce208e
Mauricio Solorio <56321447+mauricios20@users.noreply.github.com>
2022-03-28 17:07:34 -0700

    Fuga

--- Per_Market_Analysis.py
+++ Per_Market_Analysis.py
@@ -8,6 +8,44 @@ from pystata import stata
 from sfi import Scalar, Matrix
 
 
+# Function for Overall Stats
+
+
+def iso(key, x):
+    # 15 blocks takes 10 observations, decades
+    command = 'summarize '+x+', '+'detail'
+    list_of_stats = []
+    stata.pdataframe_to_data(DataFrameDict[key], force=True)
+    stata.run(command)
+    # Get stats for each block
+    N = Scalar.getValue('r(N)')
+    mean = Scalar.getValue('r(mean)')
+    min = Scalar.getValue('r(min)')
+    max = Scalar.getValue('r(max)')
+    sd = Scalar.getValue('r(sd)')
+    sum = Scalar.getValue('r(sum)')
+    var = Scalar.getValue('r(Var)')
+    kurt = Scalar.getValue('r(kurtosis)')
+    skw = Scalar.getValue('r(skewness)')
+
+    # Append results to stats list_df
+    list_of_stats.extend((N, mean, max, min, sd, var, sum, kurt, skw))
+
+    # Rename columns DO NOT FORGERT
+    return list_of_stats
+
+# Second Function for Overall Statistics
+
+
+def compile_dic(dic):
+    # Compile dictionary created above into a data frame
+    dtf = pd.DataFrame.from_dict(dic, orient='index')
+    dtf.rename(columns={0: "N", 1: "Mean", 2: "Max",
+                        3: "Min", 4: "Sd", 5: "Var",
+                        6: "Sum of Variable", 7: "Kurtosis",
+                        8: "Skewness"}, inplace=True)
+
+    return dtf
 # Function for Decades and 30 year blocks
 
 
@@ -87,7 +125,7 @@ path = '/Users/mau/Dropbox/Mac/Documents/Dissertation/Heuristics/Data'
 os.chdir(path)
 
 dtf = pd.read_csv('JSTdatasetR5.csv', header=0,
-                    usecols=['year', 'iso', 'eq_capgain', 'eq_dp'])
+                    usecols=['year', 'iso', 'eq_capgain', 'eq_dp', 'bill_rate'])
 
 # Create dataframes for each country into countries
 countries = dtf.iso.unique()
@@ -96,33 +134,61 @@ dummy_dtf = pd.DataFrame(dummy_years, columns=['year'])
 DataFrameDict = {elem: pd.DataFrame for elem in countries}
 list_df = {elem: [] for elem in countries}
 
-
+GStats = {elem: pd.DataFrame for elem in countries}
+GStatsDP = {elem: pd.DataFrame for elem in countries}
+GStatsBills = {elem: pd.DataFrame for elem in countries}
 for key in DataFrameDict.keys():
     DataFrameDict[key] = dtf[dtf['iso'] == key]
     DataFrameDict[key] = DataFrameDict[key].append(dummy_dtf, ignore_index=True)
+    GStats[key] = iso(key, 'eq_capgain')
+    GStatsDP[key] = iso(key, 'eq_dp')
+    GStatsBills[key] = iso(key, 'bill_rate')
+
+
+# # Performe stata commands for Overall Stats
+GSdtf = compile_dic(GStats)
+GSdtfDP = compile_dic(GStatsDP)
+GSdtfBills = compile_dic(GStatsBills)
+
+GSdtfDP
+
 
 # # Performe stata commands in all the decades
 DecadeDict = {elem: pd.DataFrame for elem in countries}
+DecadeDictDP = {elem: pd.DataFrame for elem in countries}
+DecadeDictBills = {elem: pd.DataFrame for elem in countries}
 for key in DecadeDict.keys():
     DecadeDict[key] = iso_dtf(key, 'eq_capgain', 15)
+    DecadeDictDP[key] = iso_dtf(key, 'eq_dp', 15)
+    DecadeDictBills[key] = iso_dtf(key, 'bill_rate', 15)
 
 DecadeDict['ITA']
 # Write the Results on a Excel Spread Sheet
 writer = pd.ExcelWriter('Decades_Output.xlsx', engine='xlsxwriter')
 for key in DecadeDict:
     DecadeDict[key].to_excel(writer, sheet_name=key)
+    DecadeDictBills[key].to_excel(writer, sheet_name=key,
+                                    startrow=0, startcol=11)
+    DecadeDictDP[key].to_excel(writer, sheet_name=key,
+                                    startrow=18, startcol=0)
 writer.save()
 
 # # Performe stata commands in all the 30 year blocks
 ThirtyDict = {elem: pd.DataFrame for elem in countries}
+ThirtyDictDP = {elem: pd.DataFrame for elem in countries}
+ThirtyDictBills = {elem: pd.DataFrame for elem in countries}
 for key in ThirtyDict.keys():
     ThirtyDict[key] = iso_dtf(key, 'eq_capgain', 5)
+    ThirtyDictDP[key] = iso_dtf(key, 'eq_dp', 5)
+    ThirtyDictBills[key] = iso_dtf(key, 'bill_rate', 5)
 
 ThirtyDict['AUS']
 # Write the Results on a Excel Spread Sheet
 writer = pd.ExcelWriter('Thirty_Output.xlsx', engine='xlsxwriter')
 for key in ThirtyDict:
     ThirtyDict[key].to_excel(writer, sheet_name=key)
+    ThirtyDictDP[key].to_excel(writer, sheet_name=key, startrow=8, startcol=0)
+    ThirtyDictBills[key].to_excel(writer, sheet_name=key, startrow=0, startcol=11)
 writer.save()
 
 # 'AUS', 'BEL', 'CAN', 'CHE', 'DEU', 'DNK', 'ESP', 'FIN', 'FRA',
@@ -130,14 +196,20 @@ writer.save()
 
 # # Performe stata commands in all the 100 year blocks
 CenturyDict = {elem: pd.DataFrame for elem in countries}
+CenturyDictDP = {elem: pd.DataFrame for elem in countries}
+CenturyDictBills = {elem: pd.DataFrame for elem in countries}
 for key in CenturyDict.keys():
     CenturyDict[key] = iso_dtfC(key, 'eq_capgain', 2)
+    CenturyDictDP[key] = iso_dtfC(key, 'eq_dp', 2)
+    CenturyDictBills[key] = iso_dtfC(key, 'bill_rate', 2)
 
 CenturyDict['AUS']
 # Write the Results on a Excel Spread Sheet
 writer = pd.ExcelWriter('Century_Output.xlsx', engine='xlsxwriter')
 for key in CenturyDict:
     CenturyDict[key].to_excel(writer, sheet_name=key)
+    CenturyDictDP[key].to_excel(writer, sheet_name=key, startrow=5, startcol=0)
+    CenturyDictBills[key].to_excel(writer, sheet_name=key, startrow=0, startcol=11)
 writer.save()
 
 
